8fc00831d5f586121e9625ba60327ca2795a2dee
ValimarTheAshenKnight <david.f.buetikofer@gmail.com>
2017-06-28 16:27:50 +0200

    Verbesserung des Projekts
    Es kann nun auf die HTML datei zugegriffen werden was es mir ermöglicht
    Film Bilder und Texte zu besagten Bildern anzuzeigen und das mit einer
    Umrandung

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,22 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -55,26 +59,47 @@ public class Main extends Application {
 
         Film.setOnAction(event -> this.MovieBildschirm.show());
 
-        DontPushMe.add(Admin, 4, 0);
-        DontPushMe.add(Film, 5, 0);
+        DontPushMe.add(Admin, 0, 0);
+        DontPushMe.add(Film, 1, 0);
 
         return DontPushMe;
     }
 
     public void createMovieBildschirm(Stage primaryStage, BorderPane mainLayout){
         BorderPane layout = new BorderPane();
-        Button seats = new Button("Hall 1");
-        seats.setPadding(new Insets(100));
-        seats.setOnAction(event -> this.Hall.show());
 
-        layout.setCenter(seats);
+        GridPane grid = new GridPane();
+        /*final ImageView selectedImage = new ImageView();
+        Image image1 = null;
+        try {
+            image1 = new Image(new FileInputStream("C:\\Users\\d.buetikofer\\Documents\\GitHub\\pics\\dungeon.jpg"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/
+
+
+        WebView browser = new WebView();
+        WebEngine engine = browser.getEngine();
+        String url = getClass().getResource("movies.html").toExternalForm();
+        engine.load(url);
+        //selectedImage.setImage(image1);
+
+        //grid.add(selectedImage, 0, 0);
+
+        StackPane sp = new StackPane();
+        BorderPane bp = new BorderPane();
+
+        bp.setCenter(browser);
+        sp.getChildren().add(bp);
+
 
         this.MovieBildschirm = new Stage();
         this.MovieBildschirm.setTitle("Movies");
         this.MovieBildschirm.initOwner(primaryStage);
         this.MovieBildschirm.initModality(Modality.WINDOW_MODAL);
-        this.MovieBildschirm.setScene(new Scene(layout,1000,1000));
+        this.MovieBildschirm.setScene(new Scene(sp,1000,1000));
 
+        //return sp;
     }
 
     public void createHall(Stage primaryStage, BorderPane mainlayout){
